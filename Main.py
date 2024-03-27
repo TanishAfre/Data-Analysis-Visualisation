f245a2caef68f1feafa1f554301333bea7d8028a
tanish <tanish.afre@gmail.com>
2024-03-27 17:34:54 +0000

    reading csv file

---
+++ Main.py
@@ -0,0 +1,14 @@
+import pandas as pd
+from sklearn.cluster import KMeans
+from sklearn.preprocessing import StandardScaler
+import numpy as np
+import seaborn as sns
+import warnings
+import matplotlib.pyplot as plt
+import plotly.graph_objects as go
+
+# Load the data into a pandas DataFrame
+data = pd.read_csv("C:\\Users\\tanis\\Data-Analysis-Visualisation\\WHR2023.csv")
+
+df = pd.read_csv("C:\\Users\\tanis\\Data-Analysis-Visualisation\\WHR2023.csv")
+
