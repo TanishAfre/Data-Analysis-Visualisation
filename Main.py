0aa7726442021fa8e999d16fa5a21bfaf5a8a446
tanish <tanish.afre@gmail.com>
2024-04-03 21:26:03 +0100

    Getting all the imports

--- Main.py
+++ Main.py
@@ -1,15 +1,18 @@
 import pandas as pd
 from sklearn.cluster import KMeans
-from sklearn.preprocessing import StandardScaler
+from sklearn.preprocessing import StandardScaler,  normalize
+from sklearn.cluster import KMeans
 import numpy as np
 import seaborn as sns
 import warnings
 import matplotlib.pyplot as plt
 import plotly.graph_objects as go
+import plotly.express as px
+import plotly.graph_objects as go
+from plotly.offline import iplot
+import warnings
 
 # Load the data into a pandas DataFrame
-data = pd.read_csv("C:\\Users\\tanis\\Data-Analysis-Visualisation\\WHR2023.csv")
-
 df = pd.read_csv("C:\\Users\\tanis\\Data-Analysis-Visualisation\\WHR2023.csv")
 
 # Display the first 5 rows of the data
